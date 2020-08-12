e2bb59f719cd969573f4db5287cc3d96af79e066
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-08-12 17:50:26 +0300

    Fixes

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ A script to notify you when new post is made to a subreddit
 ## .env
 
 export SURL=https://hooks.slack.com/services/code/code/code
+
 export REDDIT_ID=id
+
 export REDDIT_SECRET=secret
+
+SURL is Slack Webhook URL
 
 # Requirements
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import praw, requests, os
+import praw, requests, os, json
+from datetime import datetime
 
 class SubredditWatchdog:
     def __init__(self, redditId, secret, slackurl, subredditName):
@@ -13,22 +14,26 @@ class SubredditWatchdog:
             client_secret=self.secret,
             user_agent="linux:me.aleksilassila.subredditwatchdog:v1 (by u/0x3A7F14)")
 
-    def sendMessage(self, title, content):
+    def sendMessage(self, title, content, time):
         requests.post(
             self.slackurl,
-            data = json.dumps({"text": f"{title}:\n{content}"})
+            data = json.dumps({"text": f"{datetime.utcfromtimestamp(time).strftime('%d.%m %H:%M')}\n{title}:\n{content}\n\n"})
         )
         print(f"[+] Sent notification")
 
     def watch(self):
-        for postId in reddit.subreddit(self.subredditName).stream.submissions():
+        for postId in self.reddit.subreddit(self.subredditName).stream.submissions():
             print(f"[+] Caught new post: {postId}")
-            
-            title = print(reddit.submission(postId).title)
-            content = print(reddit.submission(postId).selftext)
 
-            self.sendMessage(title, content)
+            submission = self.reddit.submission(postId)
+
+            title = submission.title
+            content = submission.selftext
+            time = submission.created_utc
+
+            self.sendMessage(title, content, time)
 
 
 if __name__ == "__main__":
-    SubredditWatchdog(os.environ.get("REDDIT_ID"), os.environ.get("REDDIT_SECRET"), os.environ.get("SURL"), "TweakBounty")
+    watchdog = SubredditWatchdog(os.environ.get("REDDIT_ID"), os.environ.get("REDDIT_SECRET"), os.environ.get("SURL"), "TweakBounty")
+    watchdog.watch()
