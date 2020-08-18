f5d14c6f3ad57bc511b839a7ba6b352a847563ca
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-08-18 10:54:44 +0300

    Fixed bug that crashed program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw, requests, os, json
 from datetime import datetime
+from time import sleep
 
 class SubredditWatchdog:
     def __init__(self, redditId, secret, slackurl, subredditName):
@@ -14,24 +15,28 @@ class SubredditWatchdog:
             client_secret=self.secret,
             user_agent="linux:me.aleksilassila.subredditwatchdog:v1 (by u/0x3A7F14)")
 
-    def sendMessage(self, title, content, time):
+    def sendMessage(self, title, content, time, link):
         requests.post(
             self.slackurl,
-            data = json.dumps({"text": f"{datetime.utcfromtimestamp(time).strftime('%d.%m %H:%M')}\n{title}:\n{content}\n\n"})
+            data = json.dumps({"text": f"{datetime.utcfromtimestamp(time).strftime('%d.%m %H:%M')}\n{title}:\n{content}\nSource: {link}\n\n"})
         )
         print(f"[+] Sent notification")
 
     def watch(self):
-        for postId in self.reddit.subreddit(self.subredditName).stream.submissions():
-            print(f"[+] Caught new post: {postId}")
+        while True:
+            try:
+                for postId in self.reddit.subreddit(self.subredditName).stream.submissions():
+                    print(f"[+] Caught new post: {postId}")
 
-            submission = self.reddit.submission(postId)
+                    submission = self.reddit.submission(postId)
 
-            title = submission.title
-            content = submission.selftext
-            time = submission.created_utc
+                    title = submission.title
+                    content = submission.selftext
+                    time = submission.created_utc
+                    link = submission.url
 
-            self.sendMessage(title, content, time)
+                    self.sendMessage(title, content, time, link)
+            except: sleep(60)
 
 
 if __name__ == "__main__":
